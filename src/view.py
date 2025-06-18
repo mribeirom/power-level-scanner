f0ba9739bb6bbaa5c107ab5838628421cdd65381
mribeirom <marcelormartins0@gmail.com>
2025-06-17 21:19:46 -0300

    adicionando imagem a tela

--- src/view.py
+++ src/view.py
@@ -1,4 +1,5 @@
 import customtkinter
+from PIL import Image
 
 class View():
     def __init__(self):
@@ -7,8 +8,14 @@ class View():
         self.root.title('Força Saiyajin')
         self.root.minsize(400, 400)
 
+        self.estrutura_imagem(self.root)
+
         # Frame para ser a borda do container principal
-        self.container_borda = customtkinter.CTkFrame(self.root, width=500, height=500, corner_radius=12, fg_color="#000000")
+        self.container_borda = customtkinter.CTkFrame(self.root,
+                                                      width=500,
+                                                      height=500,
+                                                      corner_radius=12,
+                                                      fg_color="#000000")
         self.container_borda.pack(padx=45, pady=45)
 
         # Frame principal
@@ -24,13 +31,25 @@ class View():
 
         self.root.mainloop()
 
+    def estrutura_imagem(self, container):
+        container_imagem = customtkinter.CTkFrame(container, width=500, fg_color="transparent")
+        container_imagem.pack(pady=(30, 0))
+
+        imagem = Image.open('vegeta_weight_training.png')
+        imagem_ctk = customtkinter.CTkImage(light_image=imagem, size=(150, 150))
+
+        self.label_imagem = customtkinter.CTkLabel(container_imagem, image=imagem_ctk, text="")
+        self.label_imagem.pack()
+
+
+
     def estrutura_container(self, container):
         # Cria um frame para armazenar todos os componentes
         container_login = customtkinter.CTkFrame(container,
                                                  width=500,
                                                  fg_color="transparent")
         container_login.pack(padx=20,
-                             pady=(40, 0))
+                             pady=(30, 0))
         container_login.grid_propagate(False)
 
         self.estrutura_container_login(container_login)
